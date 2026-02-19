2c15161775970575cfa84062b1e365841fb182a3
hongcha1015 <106043113+hongcha1015@users.noreply.github.com>
2026-02-19 17:50:32 -0500

    Update: Real stat check for 1 selected sword and level TODO: Colour-coded comparison

--- actual_stats_calc.py
+++ actual_stats_calc.py
@@ -18,6 +18,16 @@ def get_actual_stats(character, level):
         'Scouting(偵察)': 0.02,
         'Camouflage(隠蔽)': 0.02,
     }
+    SWORD_TYPE_FIX = {
+        'Tantou': 1,
+        'Wakizashi': 1,
+        'Uchigatana': 0.65,
+        'Tachi': 0.85,
+        'Ootachi': 1,
+        'Yari': 0.6,
+        'Naginata': 0.35,
+        'Tsurugi': 1,
+    }
     LEVEL_FIX = 0.1
     
     for stat, coeff in STATS_FIX.items():
@@ -26,6 +36,9 @@ def get_actual_stats(character, level):
             actual_stats[stat] = int(base_val * (100 * coeff + (level - 100) * coeff * LEVEL_FIX + 1))
         else:
             actual_stats[stat] = int(base_val * (level * coeff + 1))
+        if 'Impact' in stat:
+            actual_stats[stat] = int(actual_stats[stat] * SWORD_TYPE_FIX.get(character['Type']))
+            
 
     return actual_stats
 
